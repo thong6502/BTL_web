5d7f766faf1328a5562c68bff6f491a1c8569f4b
hoangson <tranhoangson03@gmail.com>
2024-11-10 22:12:36 +0700

    son_sieucapdeptrai

--- app/models/sanpham.php
+++ app/models/sanpham.php
@@ -1,80 +1,85 @@
 <?php
 include($_SERVER['DOCUMENT_ROOT'] . '/BTL_web/config/dbconnect.php');
 
-  class data_sanpham
-  {
-    
-    public function hien_thi_sanpham(){
-      global $conn;
-      $sql = "SELECT * FROM tbl_sanpham";
-      $result = mysqli_query($conn, $sql);
-      return $result;
+class data_sanpham
+{
+    // Phương thức để hiển thị sản phẩm với phân trang
+    public function hien_thi_sanpham($limit, $offset)
+    {
+        global $conn;
+        // Câu lệnh SQL sử dụng LIMIT và OFFSET để phân trang
+        $sql = "SELECT * FROM tbl_sanpham LIMIT $limit OFFSET $offset";
+        $result = mysqli_query($conn, $sql);
+        return $result;
     }
 
-    public function sua_san_pham($id_sp, $id_hsx, $id_dm, $tensanpham, $giaban, $img_path, $chitiet){
-      global $conn;
-      $sql = "UPDATE tbl_sanpham SET id_hsx = $id_hsx, = , tensanpham = $tensanpham, giaban = $giaban, img_path = $img_path, chitiet = $chitiet WHERE id_sp = $id_sp";
-      $result = mysqli_query($conn, $sql);
-      if($result){
-        return true;
-      }else{
-        return false;
-      }
+    // Phương thức lấy tổng số sản phẩm
+    public function get_total_sanpham()
+    {
+        global $conn;
+        // Lấy tổng số sản phẩm (để tính tổng số trang)
+        $sql = "SELECT COUNT(*) as total FROM tbl_sanpham";
+        $result = mysqli_query($conn, $sql);
+        if ($result) {
+            $data = mysqli_fetch_assoc($result);
+            return $data['total']; // Trả về tổng số sản phẩm
+        } else {
+            return 0;
+        }
     }
 
-    public function them_san_pham($id_hsx, $id_dm, $tensanpham, $giaban, $img_path, $chitiet){
-      global $conn;
-      $sql = "INSERT INTO tbl_sanpham (id_hsx,, tensanpham, giaban, img_path, chitiet) VALUES ($id_hsx, , $tensanpham, $giaban, $img_path, $chitiet)";
-      $result = mysqli_query($conn, $sql);
-      if($result){
-        return true;
-      }else{
-        return false;
-      }
+    // Các phương thức khác (sửa, thêm, xóa, tìm kiếm, v.v.)
+    public function sua_san_pham($id_sp, $id_hsx, $id_dm, $tensanpham, $giaban, $img_path, $chitiet)
+    {
+        global $conn;
+        $sql = "UPDATE tbl_sanpham SET id_hsx = $id_hsx, tensanpham = '$tensanpham', giaban = $giaban, img_path = '$img_path', chitiet = '$chitiet' WHERE id_sp = $id_sp";
+        $result = mysqli_query($conn, $sql);
+        return $result ? true : false;
     }
 
-    public function xoa_san_pham($id_sp){
-      global $conn;
-      $sql = "UPDATE tbl_sanpham SET is_xoa = 0 WHERE id_sp = $id_sp";
-      $result = mysqli_query($conn, $sql);
-      if($result){
-        return true;
-      }else{
-        return false;
-      }
+    public function them_san_pham($id_hsx, $id_dm, $tensanpham, $giaban, $img_path, $chitiet)
+    {
+        global $conn;
+        $sql = "INSERT INTO tbl_sanpham (id_hsx, id_dm, tensanpham, giaban, img_path, chitiet) VALUES ($id_hsx, $id_dm, '$tensanpham', $giaban, '$img_path', '$chitiet')";
+        $result = mysqli_query($conn, $sql);
+        return $result ? true : false;
     }
 
-    public function khoi_phuc_san_pham($id_sp){
-      global $conn;
-      $sql = "UPDATE tbl_sanpham SET is_xoa = 1 WHERE id_sp = $id_sp";
-      $result = mysqli_query($conn, $sql);
-      if($result){
-        return true;
-      }else{
-        return false;
-      }
+    public function xoa_san_pham($id_sp)
+    {
+        global $conn;
+        $sql = "UPDATE tbl_sanpham SET is_xoa = 0 WHERE id_sp = $id_sp";
+        $result = mysqli_query($conn, $sql);
+        return $result ? true : false;
     }
 
-    public function search_sanpham($id_hsx, $tensp){
-      global $conn;
-      $sql = "SELECT * FROM tbl_sanpham WHERE id_hsx LIKE '%$id_hsx%' AND tensp LIKE '%$tensp%'";
-      $result = mysqli_query($conn, $sql);
-      return $result;
+    public function khoi_phuc_san_pham($id_sp)
+    {
+        global $conn;
+        $sql = "UPDATE tbl_sanpham SET is_xoa = 1 WHERE id_sp = $id_sp";
+        $result = mysqli_query($conn, $sql);
+        return $result ? true : false;
     }
 
-    public function lay_sanpham_theo_id($id) {
-      global $conn;
-      $sql = "SELECT * FROM tbl_sanpham WHERE id_sp = $id"; // Chỉ lấy sản phẩm chưa bị xóa
-      $result = mysqli_query($conn, $sql);
-  
-      if ($result && mysqli_num_rows($result) > 0) {
-          return mysqli_fetch_assoc($result); // Trả về dữ liệu sản phẩm dưới dạng mảng
-      } else {
-          return null; // Trả về null nếu không tìm thấy sản phẩm
-      }
-  }
-  
+    public function search_sanpham($id_hsx, $tensp)
+    {
+        global $conn;
+        $sql = "SELECT * FROM tbl_sanpham WHERE id_hsx LIKE '%$id_hsx%' AND tensp LIKE '%$tensp%'";
+        $result = mysqli_query($conn, $sql);
+        return $result;
+    }
 
+    public function lay_sanpham_theo_id($id)
+    {
+        global $conn;
+        $sql = "SELECT * FROM tbl_sanpham WHERE id_sp = $id"; // Chỉ lấy sản phẩm chưa bị xóa
+        $result = mysqli_query($conn, $sql);
 
-  }
+        if ($result && mysqli_num_rows($result) > 0) {
+            return mysqli_fetch_assoc($result); // Trả về dữ liệu sản phẩm dưới dạng mảng
+        } else {
+            return null; // Trả về null nếu không tìm thấy sản phẩm
+        }
+    }
+}
 ?>
--- app/views/user/ListProduct/Listproduct.php
+++ app/views/user/ListProduct/Listproduct.php
@@ -3,7 +3,22 @@
         <?php
         include('./../../models/sanpham.php');
         $get_data = new data_sanpham();
-        $select = $get_data->hien_thi_sanpham();
+
+        // Số sản phẩm trên mỗi trang
+        $products_per_page = 8;
+
+        // Lấy trang hiện tại từ URL, nếu không có thì mặc định là trang 1
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+
+        // Tính toán offset
+        $offset = ($page - 1) * $products_per_page;
+
+        // Lấy danh sách sản phẩm theo trang
+        $select = $get_data->hien_thi_sanpham($products_per_page, $offset);
+
+        // Lấy tổng số sản phẩm để tính số trang
+        $total_products = $get_data->get_total_sanpham();
+        $total_pages = ceil($total_products / $products_per_page);
         ?>
         
         <!-- Hiển thị thông báo thành công nếu có -->
@@ -18,8 +33,8 @@
         <?php } ?>
 
         <div class="row justify-content-start">
-            <?php if (!empty($select)) { ?>
-                <?php foreach ($select as $pr) { ?>
+            <?php if (mysqli_num_rows($select) > 0) { ?>
+                <?php while ($pr = mysqli_fetch_assoc($select)) { ?>
                     <div class="product__item col-md-4 col-sm-6 col-lg-3 mb-4">
                         <div class="box__item border p-3">
                             <a href="./DetailProduct/DetailProduct.php?id=<?php echo htmlspecialchars($pr['id_sp']); ?>" class="product__link text-decoration-none">
@@ -46,5 +61,27 @@
                 <p>Không có sản phẩm nào.</p>
             <?php } ?>
         </div>
+
+        <!-- Phân trang -->
+        <nav>
+            <ul class="pagination justify-content-center">
+                <!-- Trang trước -->
+                <?php if ($page > 1) { ?>
+                    <li class="page-item"><a class="page-link" href="?page=<?php echo $page - 1; ?>">Trước</a></li>
+                <?php } ?>
+
+                <!-- Các số trang -->
+                <?php for ($i = 1; $i <= $total_pages; $i++) { ?>
+                    <li class="page-item <?php echo ($i == $page) ? 'active' : ''; ?>">
+                        <a class="page-link" href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+                    </li>
+                <?php } ?>
+
+                <!-- Trang sau -->
+                <?php if ($page < $total_pages) { ?>
+                    <li class="page-item"><a class="page-link" href="?page=<?php echo $page + 1; ?>">Sau</a></li>
+                <?php } ?>
+            </ul>
+        </nav>
     </div>
 </main>
